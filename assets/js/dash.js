7984c85a664aec68408a589428e51d2f22435760
Vitor Henrique <vitorhenrique_rgt@outlook.com.br>
2024-06-26 09:45:09 -0300

    Reorganizando CSS

--- assets/js/dash.js
+++ assets/js/dash.js
@@ -1,6 +1,13 @@
 const burgerMenu  = document.querySelector('#burger_menu');
 const nav = document.querySelector('nav')
+const textLogo = document.querySelector('.text-logo')
 
 burgerMenu.addEventListener('click', () => {
   nav.classList.toggle("none");
+  if (nav.classList.contains("none")) {
+    textLogo.style.display="none"
+  }else{
+    textLogo.style.display="block"
+  }
+
 })
