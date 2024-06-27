4d72f1509735cbab754fd5c42a82c05b741a61c9
Vitor Henrique <vitorhenrique_rgt@outlook.com.br>
2024-06-27 10:16:47 -0300

    Refatorando projeto para um projeto react usando vite

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { GlobalStyle } from './styles/globalStyles'
+import Home from './pages/Home'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <GlobalStyle />
+    <Home/>
+  </React.StrictMode>,
+)
