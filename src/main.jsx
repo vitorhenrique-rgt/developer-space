4e1f12d5a5bb26b4788b4b35b20b968677dc62c6
Vitor Henrique <vitorhenrique_rgt@outlook.com.br>
2024-07-26 12:21:03 -0300

    :sparkles: feat: login page

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-import Home from "./pages/Home";
+import AuthLogin from "./pages/SignIn";
+// import Home from "./pages/Home";
 import { GlobalStyle } from "./styles/globalStyles";
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+	<React.StrictMode>
     <GlobalStyle />
-    <Home />
+	<GoogleOAuthProvider clientId={import.meta.env.VITE_CLIENT_ID}>
+    <AuthLogin />
+		</GoogleOAuthProvider>
   </React.StrictMode>,
 );
