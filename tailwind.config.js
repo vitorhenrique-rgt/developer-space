77b6df839ac5fb3484d8d9a38123807023a5c868
Vitor Henrique <vitorhenrique_rgt@outlook.com.br>
2024-08-03 18:11:01 -0300

    🔧 chore: change tailwind css configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}','./src/**/**/*.{js,jsx,ts,tsx}'],
+  content: [
+		'./index.css',
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx}'
+  ],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '0.5': '2px', // Adiciona um padding de 2px
+      },
+		},
   },
   plugins: [],
 }
+
 
