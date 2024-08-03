499dfe113f8d6b2a4c19d12d7f250d00f5dae8d8
Vitor Henrique <vitorhenrique_rgt@outlook.com.br>
2024-08-03 18:06:59 -0300

    ♻️ refactor: change components in the main file

--- src/main.jsx
+++ src/main.jsx
@@ -2,18 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import theme from './styles/theme'
-// import Home from "./pages/Home";
+import Home from "./pages/Home";
 import { GlobalStyle } from "./styles/globalStyles";
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import { ThemeProvider } from "styled-components";
-import AuthSignUp from "./pages/SignUp";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<ThemeProvider theme={theme}>
 			<GlobalStyle />
 			<GoogleOAuthProvider clientId={import.meta.env.VITE_CLIENT_ID}>
-				<AuthSignUp />
+				<Home />
 			</GoogleOAuthProvider>
 		</ThemeProvider>
   </React.StrictMode>,
