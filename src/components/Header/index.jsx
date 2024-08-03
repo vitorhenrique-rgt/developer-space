6eb887219234cce418e03f063c6e63eadc386d09
Vitor Henrique <vitorhenrique_rgt@outlook.com.br>
2024-08-03 17:53:21 -0300

    💄 feat: update header component styles

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -6,34 +6,37 @@ import {
 	SearchContainer,
 	SwitchMode,
 	UserInfoContainer,
-} from "./style";
+	IconTheme} from "./style";
 import Flag from "../../assets/brasil.png";
 import Profile from "../../assets/avatar.png";
 import Input from "../Input";
+import Icon from "../Icon";
 
 function Header() {
 	return (
 		<HeaderContainer>
 			<SearchContainer>
-				<span className="material-symbols-rounded">menu</span>
+				<Icon color={'gray'} size="lg">menu</Icon>
+				<div className="relative">
 				<Input
-					iconType={"search"}
-					inputType={"search"}
-					side={"right"}
-					info={"Pesquisar..."}
+					type={"search"}
+					side={"left"}
+					placeholder={"Pesquisar..."}
 				></Input>
+				<Icon side={"left"} size="lg">search</Icon>
+				</div>
 			</SearchContainer>
 			<UserInfoContainer>
 				<LanguageContainer>
 					<FlagImage src={Flag} />
-					<span>Português</span>
-					<span className="material-symbols-rounded">keyboard_arrow_down</span>
+					<span className="text-gray-400">Português</span>
+					<Icon size='lg'>keyboard_arrow_down</Icon>
 				</LanguageContainer>
-				<span className="material-symbols-rounded">apps</span>
-				<span className="material-symbols-rounded">notifications</span>
+				<Icon size='lg' className="material-symbols-rounded">apps</Icon>
+				<Icon size='lg' className="material-symbols-rounded">notifications</Icon>
 				<SwitchMode>
-					<span className="material-symbols-rounded">light_mode</span>
-					<span className="material-symbols-rounded">dark_mode</span>
+					<IconTheme size='md' className="material-symbols-rounded">light_mode</IconTheme>
+					<IconTheme size='md' className="material-symbols-rounded bg-gray-800">dark_mode</IconTheme>
 				</SwitchMode>
 				<ProfileImage src={Profile} />
 			</UserInfoContainer>
